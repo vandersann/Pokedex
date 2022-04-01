f88af1a7d014365d89f2c89e1aab19e79dd58fff
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2022-04-01 20:58:20 -0300

    Criação do Template e adição de background fixo

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style>
+#app{
+  background: linear-gradient(
+      to bottom right,
+      #F3DAED,
+      #B7A3E0
+    )
+    no-repeat center center fixed !important;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
+}
+</style>
