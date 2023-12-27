6ee311357c8c96a23c16cc20d2464b70655893a5
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2023-12-27 19:54:20 -0300

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 module.exports = {
   publicPath: "/Pokedex",
   transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    mode: 'development',
+    devtool: false,
+    optimization: {
+      splitChunks: {
+
+
+        chunks: 'all',
+        minSize: 15000,
+        maxSize: 250000,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        enforceSizeThreshold: 50000,
+
+      },
+
+
+    },
+
+  },
 };
